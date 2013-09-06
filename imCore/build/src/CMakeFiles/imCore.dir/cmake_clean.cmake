d6b50c8ed0facef0f7b8467f62bab045490c93ce
sol <sol.linux@gmail.com>
2013-09-06 18:29:37 +0100

    Add Texture class to imCore

---
+++ imCore/build/src/CMakeFiles/imCore.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/imCore.dir/Graphics/Texture.cpp.obj"
+  "libimCore.pdb"
+  "libimCore.dll"
+  "libimCore.dll.a"
+  "libimCore.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/imCore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ imCore/src/Graphics/Texture.h
@@ -0,0 +1,12 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+/**
+ * @brief Класс Texture - основной, базовый класс для работы с текстурами.
+ */
+class Texture {
+public:
+        Texture();
+};
+
+#endif // TEXTURE_H
