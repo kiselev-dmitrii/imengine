61615e7775a2f54b3541da4a95cc00fb6ba04066
sol <sol.linux@gmail.com>
2013-09-30 19:08:36 +0100

    Add VertexArray class

---
+++ src/imCore/Graphics/Buffer/VertexArray.cpp
@@ -0,0 +1,36 @@
+#include "VertexArray.h"
+#include "../../Utils/Debug.h"
+
+namespace imCore {
+
+GLuint VertexArray::s_boundHandle = 0;
+
+void VertexArray::create() {
+        IM_GLCALL(glGenVertexArrays(1, &m_handle));
+        IM_LOG("VertexArray" << m_handle << ": created");
+}
+
+void VertexArray::destroy() {
+        IM_ASSERT(m_handle);
+
+        IM_GLCALL(glDeleteVertexArrays(1, &m_handle));
+        IM_LOG("VertexArray" << m_handle << ": destroyed");
+}
+
+GLuint VertexArray::handle() {
+        return m_handle;
+}
+
+void VertexArray::bind() {
+        if (m_handle != s_boundHandle) {
+                IM_GLCALL(glBindVertexArray(m_handle));
+                s_boundHandle = m_handle;
+        }
+}
+
+void VertexArray::unbind() {
+        IM_GLCALL(glBindVertexArray(0)) ;
+        s_boundHandle = 0;
+}
+
+} //namespace imCore
