c5d218b1c5646fc73933ee22b8a6bd8b33140730
sol <sol.linux@gmail.com>
2013-09-30 15:51:54 +0100

    Add IndexBuffer implementation

--- src/imCore/Graphics/Buffer/IndexBuffer.cpp
+++ src/imCore/Graphics/Buffer/IndexBuffer.cpp
@@ -1,8 +1,24 @@
 #include "IndexBuffer.h"
+#include "../../Utils/Debug.h"
 
 namespace imCore {
 
-IndexBuffer::IndexBuffer() {
+GLuint IndexBuffer::s_boundHandle = 0;
+
+void IndexBuffer::create() {
+        BufferObject::create(BufferTarget::ELEMENT_ARRAY);
+}
+
+void IndexBuffer::bind() {
+        if (m_handle != s_boundHandle) {
+                IM_GLCALL(glBindBuffer(m_target, m_handle));
+                s_boundHandle = m_handle;
+        }
+}
+
+void IndexBuffer::unbind() {
+        IM_GLCALL(glBindBuffer(m_target, 0));
+        s_boundHandle = 0;
 }
 
 } //namespace imCore
--- src/imCore/Graphics/Buffer/IndexBuffer.h
+++ src/imCore/Graphics/Buffer/IndexBuffer.h
@@ -1,6 +1,8 @@
 #ifndef INDEXBUFFER_H
 #define INDEXBUFFER_H
 
+#include "BufferObject.h"
+
 namespace imCore {
 
 /** @brief Класс предназначен для хранения индексов.
@@ -8,9 +10,19 @@ namespace imCore {
  *  Индексы определяют порядок взятия очередной вершины из вершинного буфера,
  *  и используются при отрисовке сложных полигональных моделей.
  */
-class IndexBuffer {
+class IndexBuffer : public BufferObject {
 public:
-        IndexBuffer();
+        /// Создает индесный буфер
+        void            create();
+
+        /// Привязывает буфер
+        void            bind();
+        /// Отвязывает буфер
+        void            unbind();
+
+private:
+        static GLuint   s_boundHandle;
+
 };
 
 } //namespace imCore
