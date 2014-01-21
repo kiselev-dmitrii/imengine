f277a16f195e6828b9343d5ccc7d8d60ee1c6b94
Kiselev Dmitrii <sol.linux@gmail.com>
2014-01-21 13:20:38 +0400

    Something ideas about BoxLayout

--- src/imEngine/Graphics/GUI/BoxLayout.cpp
+++ src/imEngine/Graphics/GUI/BoxLayout.cpp
@@ -2,8 +2,4 @@
 
 namespace imEngine {
 
-BoxLayout::BoxLayout()
-{
-}
-
 } //namespace imEngine
--- src/imEngine/Graphics/GUI/BoxLayout.h
+++ src/imEngine/Graphics/GUI/BoxLayout.h
@@ -1,11 +1,49 @@
 #ifndef BOXLAYOUT_H
 #define BOXLAYOUT_H
 
+#include "Widget.h"
+
 namespace imEngine {
 
-class BoxLayout {
+
+/** @brief Используется для группировки виджетов в горизонтальные строки
+ * Добавление виджетов следует осуществлять через метод addWidget,
+ * так как при этом можно задать дополнительную информацию.
+ * При этом виджеты также должны быть сыновьями HBoxLayout
+ */
+class HBoxLayout : public WidgetAbstract, public HStretchableAbstract {
 public:
-        BoxLayout();
+        /// Конструктор
+        HBoxLayout(WidgetAbstract* parent);
+
+        /// Устанавливает выравнивание по умолчанию
+        void    setDefaultAlignment(WidgetHAlignment::Enum hAlignment, WidgetVAlignment::Enum vAlignment);
+
+        /// Добавляет виджет
+        void    addWidget(WidgetAbstract* widget, WidgetHAlignment::Enum horizontalAlignment, WidgetVAlignment::Enum verticalAlignment);
+        void    addWidget(WidgetAbstract* widget);
+
+        /// Сворачивает виджет виджет
+        void    foldWidget();
+
+        /// Добавляет пустое пространство
+        void    addSpacing(float space);
+
+
+        /// Вставляет виджет в позицию position
+        void    insertWidget(WidgetAbstract* widget, uint position);
+        /// Вставляет пустое пространство в позицию position
+        void    insertSpacing(float space, uint position);
+
+private:
+        struct Element {
+                WidgetAbstract*         widget;
+                WidgetHAlignment::Enum  hAlignment;
+                WidgetVAlignment::Enum  vAlignment;
+                float                   width;
+        };
+
+        std::vector<Element>            m_elements;
 };
 
 } //namespace imEngine
