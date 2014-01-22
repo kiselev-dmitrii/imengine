3d1e46d73d3c0550f208684aeaf590601b116e72
Kiselev Dmitrii <sol.linux@gmail.com>
2014-01-22 13:37:00 +0400

    Modified BoxLayoutTest

--- tests/BoxLayoutTest.cpp
+++ tests/BoxLayoutTest.cpp
@@ -3,6 +3,7 @@
 #include <imEngine/Graphics/GUI/Button.h>
 #include <imEngine/Graphics/GUI/Panel.h>
 #include <imEngine/Graphics/GUI/BoxLayout.h>
+#include <imEngine/Graphics/GUI/Slider.h>
 #include <imEngine/Utils/Debug.h>
 
 using namespace imEngine;
@@ -22,10 +23,9 @@ protected:
 private:
         GuiManager*     m_gui;
         Panel*          m_pnl;
-        Button*         m_btn1;
-        Button*         m_btn2;
-        Button*         m_btn3;
-        Button*         m_btn4;
+        Button*         m_btnPlus;
+        Button*         m_btnMinus;
+        HSlider*        m_slider;
         HBoxLayout*     m_lt;
 };
 
@@ -37,30 +37,23 @@ void Application::initialize() {
 
         // Создаем панельку
         m_pnl = new Panel("regular_panel.png", m_gui->root());
-        m_pnl->setSize(Vec2(400, 500));
+        m_pnl->setSize(Vec2(700, 500));
 
         // Создаем HBoxLayout
         m_lt = new HBoxLayout(m_pnl);
         m_lt->setPosition(Vec2(100, 200));
 
-        // Создание кнопок и добавление в m_lt
-        m_btn1 = new Button("regular_btn_active.png", "regular_btn_hover.png", "regular_btn_pressed.png", "regular_btn_disabled.png", "regular_btn_focused.png", m_lt);
-        m_lt->addWidget(m_btn1);
+        // Создание кнопок и слайдера
+        m_btnMinus = new Button("regular_btn_active.png", "regular_btn_hover.png", "regular_btn_pressed.png", "regular_btn_disabled.png", "regular_btn_focused.png", m_lt);
+        m_lt->addWidget(m_btnMinus, WidgetVAlignment::CENTER);
 
-        m_lt->addStretch(0.5);
+        m_slider = new HSlider("slider_background.png", "slider_selection.png", "slider_btn_active.png", "slider_btn_hover.png", m_lt);
+        m_slider->setWidth(300);
+        m_lt->addWidget(m_slider, WidgetVAlignment::CENTER);
 
-        m_btn2 = new Button("regular_btn_active.png", "regular_btn_hover.png", "regular_btn_pressed.png", "regular_btn_disabled.png", "regular_btn_focused.png", m_lt);
-        m_lt->addWidget(m_btn2);
+        m_btnPlus = new Button("regular_btn_active.png", "regular_btn_hover.png", "regular_btn_pressed.png", "regular_btn_disabled.png", "regular_btn_focused.png", m_lt);
+        m_lt->addWidget(m_btnPlus, WidgetVAlignment::CENTER);
 
-        m_lt->addStretch(1);
-
-        m_btn3 = new Button("regular_btn_active.png", "regular_btn_hover.png", "regular_btn_pressed.png", "regular_btn_disabled.png", "regular_btn_focused.png", m_lt);
-        m_lt->addWidget(m_btn3);
-
-        m_lt->addSpacing(30);
-
-        m_btn4 = new Button("regular_btn_active.png", "regular_btn_hover.png", "regular_btn_pressed.png", "regular_btn_disabled.png", "regular_btn_focused.png", m_lt);
-        m_lt->addWidget(m_btn4);
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -94,7 +87,7 @@ void Application::mouseReleaseEvent(int x, int y, char button) {
 void Application::keyPressEvent(int key) {
         if (key == '-') m_lt->setWidth(m_lt->width()-1);
         if (key == '=') m_lt->setWidth(m_lt->width()+1);
-        if (key == '1') m_btn2->setVisible(!m_btn2->isVisible());
+        if (key == '1') m_slider->setVisible(!m_slider->isVisible());
 }
 
 int main() {
