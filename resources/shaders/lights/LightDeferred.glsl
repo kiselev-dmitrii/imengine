9e2ac8c2bb3640230df1da15205eef893830bb60
Kiselev Dmitrii <sol.linux@gmail.com>
2014-03-22 11:04:34 +0400

    Modify shader

--- resources/shaders/lights/LightDeferred.glsl
+++ resources/shaders/lights/LightDeferred.glsl
@@ -2,9 +2,23 @@
 layout (location = 0) in vec2 aPosition;	// in [-1;1]x[-1;1]x[0;0]
 
 out vec2 vTexCoord;
+out vec3 vViewRay;
+
+uniform float uFarDistance;
+uniform float uAspectRatio;
+uniform float uTanFovyDiv2;
 
 void main() {
-	vTexCoord = 0.5*aPosition + vec2(0.5);
+	vTexCoord = 0.5*aPosition + 0.5;
+
+        // Создание луча направленного до очередной точки на farPlane
+        vViewRay = vec3(
+                aPosition.x * uAspectRatio * uFarDistance * uTanFovyDiv2,
+                aPosition.y * uFarDistance * uTanFovyDiv2,
+                -uFarDistance
+        );
+        vViewRay = normalize(vViewRay);
+
         gl_Position = vec4(aPosition, 0.0, 1.0);
 }
 
@@ -12,6 +26,7 @@ void main() {
 ///### FRAGMENT SHADER ###///
 
 in vec2 vTexCoord;
+in vec3 vViewRay;
 
 layout (location = 0) out vec4 fLightBuffer;
 
@@ -20,7 +35,27 @@ uniform sampler2D uGBufferMaterial;
 uniform sampler2D uGBufferGeometry;
 uniform sampler2D uGBufferDepth;
 
-void main() {
+struct Light {
+        vec3 diffuse;
+        vec3 specular;
+        vec3 positionVS;
+};
+uniform Light uLight;
 
-	fLightBuffer = vec4(vTexCoord, 0.0, 1.0);
+void main() {
+        vec3 viewRay = normalize(vViewRay);
+        float depth = texture2D(uGBufferDepth, vTexCoord).r;
+
+        /// Реконструируем позицию каждого пикселя
+        vec3 positionVS = vViewRay * depth;
+        vec3 normalVS = texture2D(uGBufferGeometry, vTexCoord).xyz * 2.0 - 1.0;
+
+        /// Вычисляем освещение по фонгу
+        vec3 s = normalize(uLight.positionVS - positionVS);
+        vec3 v = normalize(-positionVS);
+        vec3 diffuseColor =     texture2D(uGBufferDiffuse, vTexCoord).rgb * 
+                                uLight.diffuse *
+                                max(dot(s, normalVS), 0.0);
+
+	fLightBuffer = vec4(positionVS, 1.0);
 }
--- src/imEngine/Graphics/Scene/Objects/Light/PointLight.cpp
+++ src/imEngine/Graphics/Scene/Objects/Light/PointLight.cpp
@@ -11,6 +11,23 @@ PointLight::PointLight(Object *parent) :
 void PointLight::bind(Scene* scene) {
         m_program->bind();
 
+        Camera* cam = scene->activeCamera();
+        float farDistance = cam->farDistance();
+        float aspectRatio = cam->aspectRatio();
+        float tanFovyDiv2 = glm::tan(glm::radians(cam->fieldOfView() / 2));
+
+        m_program->setUniform("uFarDistance", farDistance);
+        m_program->setUniform("uAspectRatio", aspectRatio);
+        m_program->setUniform("uTanFovyDiv2", tanFovyDiv2);
+
+        Vec3 diffuseColor = m_color;
+        Vec3 specularColor = Vec3(1.0);
+        Vec3 positionVS = Vec3(cam->worldToLocalMatrix() * Vec4(worldPosition(), 1.0));
+
+        m_program->setUniform("uLight.diffuse", diffuseColor);
+        m_program->setUniform("uLight.specular", specularColor);
+        m_program->setUniform("uLight.positionVS", positionVS);
+
 }
 
 void PointLight::unbind() {
